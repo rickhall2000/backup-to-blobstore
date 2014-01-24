d1e576eca00a3fc8909a7878a20342793bb57031
unknown <Rick@Titan.(none)>
2014-01-24 10:41:42 -0500

    first working version

--- src/filer/core.clj
+++ src/filer/core.clj
@@ -1,33 +1,26 @@
 (ns filer.core
   (:require [filer.config :as config]
-            [filer.blobstore :as store])
+            [filer.blobstore :as store]
+            [filer.filestore :as files])
   (:import  [java.io File FileInputStream FileOutputStream])
   )
 
-(def directory (clojure.java.io/file config/root-folder))
-(def files (filter #(.isFile %) (file-seq directory)))
-
-(defn get-info [file]
-  {:last-mod (.lastModified file)
-     :fullname (.toString file)})
-
-(def avg-date
-  (/ (reduce +
-             (map :last-mod work-list))
-     (count work-list)))
-
-(def new-list
-  (filter (fn [x] (< (:last-mod x) avg-date)) work-list))
+(def container (store/container config/conn-str "mycontainer"))
 
 (defn upload-settings [f]
   {:file f
    :target (subs (.toString f) (inc (count config/root-folder)))})
 
-(def container (store/container config/conn-str "mycontainer"))
-
-(doseq [f (map upload-settings files)]
-  ((:upload container) f))
+(defn upload-file [file container]
+  ((:upload container) (upload-settings file)))
 
 (defn delete-blobs [ctr]
   (doseq [f ((ctr :blob-seq))]
     ((:delete ctr) f)))
+
+(defn backup-folder [folder container]
+  (doseq [f (files/all-files folder)]
+    (upload-file f container)))
+
+(defn -main [args]
+  (backup-folder config/root-folder container))
---
+++ src/filer/filestore.clj
@@ -0,0 +1,10 @@
+(fs filer.filestore
+    (:import [java.io. File]))
+
+(defn all-files [directory]
+  (filter #(.isFile %)
+          (file-seq (clojure.java.io/file directory))))
+
+(defn get-info [file]
+  {:last-mod (.lastModified file)
+     :fullname (.toString file)})
