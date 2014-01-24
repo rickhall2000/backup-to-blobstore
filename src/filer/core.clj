3f8c80caebe53ecfe69dad52715b0166abfbdc79
Rick Hall <rickhall2000@hotmail.com>
2014-01-24 08:47:30 -0500

    Continuing to refactor

--- src/filer/core.clj
+++ src/filer/core.clj
@@ -1,8 +1,7 @@
 (ns filer.core
   (:require [filer.config :as config]
             [filer.blobstore :as store])
-  (:import  [java.io File FileInputStream FileOutputStream]
-            [com.microsoft.windowsazure.services.blob.client CloudBlockBlob])
+  (:import  [java.io File FileInputStream FileOutputStream])
   )
 
 (def directory (clojure.java.io/file config/root-folder))
@@ -12,12 +11,6 @@
   {:last-mod (.lastModified file)
      :fullname (.toString file)})
 
-(defn upload-settings [f]
-  {:file f
-   :target (subs (.toString f) (inc (count config/root-folder)))})
-
-
-
 (def avg-date
   (/ (reduce +
              (map :last-mod work-list))
@@ -26,27 +19,15 @@
 (def new-list
   (filter (fn [x] (< (:last-mod x) avg-date)) work-list))
 
+(defn upload-settings [f]
+  {:file f
+   :target (subs (.toString f) (inc (count config/root-folder)))})
+
 (def container (store/container config/conn-str "mycontainer"))
 
 (doseq [f (map upload-settings files)]
   ((:upload container) f))
 
-(def on-server ((container :blob-seq)))
-
-(def on-server ((container :list-blobs) ))
-on-server
-
-
-
-(def ctr (container :ctr))
-
-(container :ctr)
-
-(.listBlobs ctr)
-
-(map #(type %) on-server)
-
-(doseq [f  (filter #(instance? CloudBlockBlob %)  on-server)]
-  ((:delete container) f))
-
-(count on-server)
+(defn delete-blobs [ctr]
+  (doseq [f ((ctr :blob-seq))]
+    ((:delete ctr) f)))
